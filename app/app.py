2b0166603486ca649f65133345f4dd0ac3ef85c4
Peter Thelwell <pthelwell55@gmail.com>
2023-05-18 20:48:55 -0500

    Made an attempt

--- app/app.py
+++ app/app.py
@@ -7,9 +7,11 @@ This file contains the routes for your application.
 
 from flask import Flask
 from flask import request, make_response, flash, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 import pickle
 import librosa
@@ -20,7 +22,6 @@ from scipy import signal
 
 from werkzeug.utils import secure_filename
 import os
-import psycopg2
 import keras
 
 # these libraries deal with mp3 to wav conversion
@@ -48,120 +49,17 @@ gSampleRate = 7000
 def server():
     return "Connected to server"
 
-
-"""@app.route('/record', methods=["POST"])
+"""@app.route('/record', methods=["GET", "POST"])
 def record():
     try:
-        cnx = psycopg2.connect(user='respalyzer', password='pa$$w0rd', host='localhost', database='respalyzer')
-        cursor = cnx.cursor()
-        file = request.files['recording']
-       
-        #f"INSERT INTO public.recordings (recording, reading, date_recorded, user_id) VALUES ('{recording}','{reading}', NOW(), (SELECT MAX(user_id) FROM \"user\"))"        
-        with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as temp:
-            temp_filename = temp.name
-            file.save(temp_filename)
-
-            # Your model loading code here
-            model = pickle.load(open('../model.pkl', 'rb'))
-
-            # Your resampling function
-            resampled_audio = resample_audio(temp_filename)
-
-            upperCutoffFreq = 3000
-            cutoffFrequencies = [80, upperCutoffFreq]
-            highPassCoeffs = signal.firwin(401, cutoffFrequencies, fs=gSampleRate, pass_zero="bandpass")
-
-            normalized_audio = normalizeVolume(applyHighpass(resampled_audio, highPassCoeffs))
-            cleaned_audio = applyLogCompressor(normalized_audio, 30)
-
-            soundfile.write(temp_filename, cleaned_audio, gSampleRate)
-
-            features = np.array(audio_features(temp_filename))
-            features_reshaped = np.reshape(features, (1, 193, 1))
-            predictions = model.predict(features_reshaped)
-            outcome, percentage, largest_index = process_predictions(predictions)
-        os.remove(temp_filename)
-        
-        cursor.execute(f"INSERT INTO public.recordings (recording, reading, date_recorded, user_id, disease_id) VALUES ('{file.filename}','{outcome}', NOW(), (SELECT MAX(user_id) FROM \"user\"), {largest_index})")
-        cnx.commit()
-        cursor.close()
-        cnx.close()
-        return make_response({"success" : "Recording added"}, 201)
-    except Exception as e:
-        return make_response({'error': str(e)}, 400)
-    
-def resample_audio(filename):
-    audioBuffer, nativeSampleRate = librosa.load(filename, dtype=np.float32, mono=True, sr=None)
-        
-    if nativeSampleRate == gSampleRate:
-        print("Resample Ran")
-        return audioBuffer
-    else:
-        duration = len(audioBuffer) / nativeSampleRate 
-        nTargetSamples = int(duration * gSampleRate)
-        timeXSource = np.linspace(0, duration, len(audioBuffer), dtype=np.float32)
-        timeX = np.linspace(0, duration, nTargetSamples, dtype=np.float32)
-        resampledBuffer = np.interp(timeX, timeXSource, audioBuffer)
-        print("Resample Ran")
-        return resampledBuffer
-
-def normalizeVolume(npArr):
-    minAmp, maxAmp = (np.amin(npArr), np.amax(npArr))
-    maxEnv = max(abs(minAmp), abs(maxAmp))
-    scale = 1.0 / maxEnv
-    npArr *= scale
-    print("Normalize Ran")
-    return npArr
-
-def applyLogCompressor(signal, gamma):
-    sign = np.sign(signal)
-    absSignal = 1 + np.abs(signal) * gamma
-    logged = np.log(absSignal)
-    scaled = logged * (1 / np.log(1.0 + gamma))
-    print("Log Compressor Ran")
-    return sign * scaled
-
-def applyHighpass(npArr, highPassCoeffs):
-    print("1 Ran")
-    return signal.lfilter(highPassCoeffs, [1.0], npArr)
-
-def audio_features(filename):
-    sound, sample_rate = librosa.load(filename)
-    stft = np.abs(librosa.stft(sound))  
-
-    mfccs = np.mean(librosa.feature.mfcc(y=sound, sr=sample_rate, n_mfcc=40), axis=1)
-    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate), axis=1)
-    mel = np.mean(librosa.feature.melspectrogram(y=sound, sr=sample_rate), axis=1)
-    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate), axis=1)
-    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(sound), sr=sample_rate), axis=1)
-
-    concat = np.concatenate((mfccs, chroma, mel, contrast, tonnetz))
-    print("2 ran")
-    return concat
-
-def process_predictions(predictions):
-    readings = ["COPD", "Healthy", "URTI", "Bronchiectasis", "Pneumonia", "Bronchiolitis"]
-    print(predictions)
-    largest_index = np.argmax(predictions)
-    print(largest_index)
-    percentage = predictions[0][largest_index]
-    print(percentage)
-    outcome = readings[largest_index]
-    print(outcome)
-    print("3 ran")
-    return outcome, percentage, largest_index"""
-
-@app.route('/record', methods=["POST"])
-def record():
-    try:
-        cnx = psycopg2.connect(user='respalyzer', password='pa$$w0rd', host='localhost', database='respalyzer')
-        cursor = cnx.cursor()
         content = request.json
         recording = content['recording']
 
+        # Load the trained AI model
         model = pickle.load(open('../model.pkl','rb'))
 
 
+        # Preprocess the recording
         resampled_audio = resample_audio(recording)
 
         upperCutoffFreq = 3000
@@ -170,22 +68,30 @@ def record():
 
         normalized_audio = normalizeVolume(applyHighpass(resampled_audio, highPassCoeffs))
         cleaned_audio = applyLogCompressor(normalized_audio, 30)
+        
+        # Save the preprocessed audio to a temporary file
         with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as temp:
             temp_filename = temp.name
             soundfile.write(temp_filename, cleaned_audio, gSampleRate)
 
+            # Extract features from the audio
             features = np.array(audio_features(temp_filename))
             features_reshaped = np.reshape(features, (1, 193, 1))
+
+            # Make predictions using the AI model
             predictions = model.predict(features_reshaped)
             outcome, percentage, largest_index = process_predictions(predictions)
             likelihood = round((percentage * 100), 2)
+
+        # Remove the temporary audio file
         os.remove(temp_filename)
 
-        cursor.execute(f"INSERT INTO public.recordings (recording, reading, date_recorded, user_id, disease_id, likelihood) VALUES ('{recording}','{outcome}', NOW(), (SELECT MAX(user_id) FROM \"user\"), {largest_index}, {likelihood})")
-        cnx.commit()
-        cursor.close()
-        cnx.close()
-        return make_response({"success" : "Recording added"}, 201)
+        # Return the response as JSON
+        response = {
+            "outcome": outcome,
+            "likelihood": likelihood
+        }
+        return make_response(response, 200)
     except Exception as e:
         return make_response({'error': str(e)}, 400)
 
@@ -248,7 +154,127 @@ def process_predictions(predictions):
     outcome = readings[largest_index]
     print(outcome)
     print("3 ran")
+    return outcome, percentage, largest_index"""
+    
+
+@app.route('/record', methods=["GET", "POST"])
+def record():
+    try:
+        if request.method == 'POST':
+            # Get the uploaded file
+            print(request.files)
+            print(request.files['recording'])
+            file = request.files['recording']
+            content_type = file.mimetype
+            print(content_type)
+            
+            # Save the file to a temporary location
+            temp_filename = tempfile.mktemp(suffix='.wav', dir='audio')
+            file.save(temp_filename)
+
+            # Load the trained AI model
+            model = pickle.load(open('../model.pkl', 'rb'))
+
+            # Preprocess the recording
+            resampled_audio = resample_audio(temp_filename)
+            # Rest of the preprocessing steps...
+            upperCutoffFreq = 3000
+            cutoffFrequencies = [80, upperCutoffFreq]
+            highPassCoeffs = signal.firwin(401, cutoffFrequencies, fs=gSampleRate, pass_zero="bandpass")
+
+            normalized_audio = normalizeVolume(applyHighpass(resampled_audio, highPassCoeffs))
+            cleaned_audio = applyLogCompressor(normalized_audio, 30)
+            
+            with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as temp:
+                temp_filename = temp.name
+                soundfile.write(temp_filename, cleaned_audio, gSampleRate)
+
+                # Extract features from the audio
+                features = np.array(audio_features(temp_filename))
+                features_reshaped = np.reshape(features, (1, 193, 1))
+
+                # Make predictions using the AI model
+                predictions = model.predict(features_reshaped)
+                outcome, percentage, largest_index = process_predictions(predictions)
+                likelihood = round((percentage * 100), 2)
+
+            # Remove the temporary audio file
+            os.remove(temp_filename)
+
+            # Return the response as JSON
+            response = {
+                "outcome": outcome,
+                "likelihood": likelihood
+            }
+            return jsonify(response), 200
+
+        # Handle GET request or other methods
+        # ...
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+def resample_audio(filename):
+    audioBuffer, nativeSampleRate = librosa.load(filename, dtype=np.float32, mono=True, sr=None)
+
+    if nativeSampleRate == gSampleRate:
+        print("Resample Ran")
+        return audioBuffer
+    else:
+        duration = len(audioBuffer) / nativeSampleRate
+        nTargetSamples = int(duration * gSampleRate)
+        timeXSource = np.linspace(0, duration, len(audioBuffer), dtype=np.float32)
+        timeX = np.linspace(0, duration, nTargetSamples, dtype=np.float32)
+        resampledBuffer = np.interp(timeX, timeXSource, audioBuffer)
+        print("Resample Ran")
+        return resampledBuffer
+
+def normalizeVolume(npArr):
+    minAmp, maxAmp = (np.amin(npArr), np.amax(npArr))
+    maxEnv = max(abs(minAmp), abs(maxAmp))
+    scale = 1.0 / maxEnv
+    npArr *= scale
+    print("Normalize Ran")
+    return npArr
+
+def applyLogCompressor(signal, gamma):
+    sign = np.sign(signal)
+    absSignal = 1 + np.abs(signal) * gamma
+    logged = np.log(absSignal)
+    scaled = logged * (1 / np.log(1.0 + gamma))
+    print("Log Compressor Ran")
+    return sign * scaled
+
+def applyHighpass(npArr, highPassCoeffs):
+    print("1 Ran")
+    return signal.lfilter(highPassCoeffs, [1.0], npArr)
+
+def audio_features(filename):
+    sound, sample_rate = librosa.load(filename)
+    stft = np.abs(librosa.stft(sound)) 
+
+    mfccs = np.mean(librosa.feature.mfcc(y=sound, sr=sample_rate, n_mfcc=40), axis=1)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate), axis=1)
+    mel = np.mean(librosa.feature.melspectrogram(y=sound, sr=sample_rate), axis=1)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate), axis=1)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(sound), sr=sample_rate), axis=1)
+
+    concat = np.concatenate((mfccs, chroma, mel, contrast, tonnetz))
+    print("2 ran")
+    return concat
+
+def process_predictions(predictions):
+    readings = ["COPD", "Healthy", "URTI", "Bronchiectasis", "Pneumonia", "Bronchiolitis"]
+    print(predictions)
+    largest_index = np.argmax(predictions)
+    print(largest_index)
+    percentage = predictions[0][largest_index]
+    print(percentage)
+    outcome = readings[largest_index]
+    print(outcome)
+    print("3 ran")
     return outcome, percentage, largest_index
+
 
 ###
 # The functions below should be applicable to all Flask apps.
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,170 @@
+function convertBlobToWav(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      audioContext.decodeAudioData(reader.result).then((audioBuffer) => {
+        // Create an OfflineAudioContext with the same sample rate as the Opus audio
+        const offlineContext = new OfflineAudioContext({
+          numberOfChannels: audioBuffer.numberOfChannels,
+          length: audioBuffer.length,
+          sampleRate: audioBuffer.sampleRate,
+        });
+
+        // Create a source node from the Opus audio buffer
+        const source = offlineContext.createBufferSource();
+        source.buffer = audioBuffer;
+
+        // Connect the source to an AudioWorkletNode for the conversion
+        offlineContext.audioWorklet.addModule('path/to/your/worker-script.js').then(() => {
+          const workletNode = new AudioWorkletNode(offlineContext, 'worker-processor');
+          source.connect(workletNode);
+          workletNode.connect(offlineContext.destination);
+
+          // Process the audio buffer to convert it to WAV format
+          workletNode.onprocessorerror = (event) => {
+            reject(event);
+          };
+
+          workletNode.onprocessorready = () => {
+            const channelData = [];
+            for (let channel = 0; channel < audioBuffer.numberOfChannels; channel++) {
+              channelData.push(audioBuffer.getChannelData(channel));
+            }
+            const interleaved = interleave(channelData);
+            const buffer = createWavBuffer(interleaved, audioBuffer.sampleRate);
+            resolve(new Blob([buffer], { type: 'audio/wav' }));
+          };
+
+          // Start the offline rendering
+          source.start();
+          offlineContext.startRendering();
+        }).catch((error) => {
+          reject(error);
+        });
+      });
+    };
+    reader.readAsArrayBuffer(blob);
+  });
+}
+
+function interleave(channelData) {
+  const channelCount = channelData.length;
+  const sampleCount = channelData[0].length;
+  const interleaved = new Float32Array(sampleCount * channelCount);
+  for (let sample = 0; sample < sampleCount; sample++) {
+    for (let channel = 0; channel < channelCount; channel++) {
+      interleaved[sample * channelCount + channel] = channelData[channel][sample];
+    }
+  }
+  return interleaved;
+}
+
+function createWavBuffer(interleaved, sampleRate) {
+  const buffer = new ArrayBuffer(44 + interleaved.length * 2);
+  const view = new DataView(buffer);
+
+  // RIFF identifier
+  writeString(view, 0, 'RIFF');
+  // RIFF chunk length
+  view.setUint32(4, 36 + interleaved.length * 2, true);
+  // RIFF type
+  writeString(view, 8, 'WAVE');
+  // format chunk identifier
+  writeString(view, 12, 'fmt ');
+  // format chunk length
+  view.setUint32(16, 16, true);
+  // sample format (PCM)
+  view.setUint16(20, 1, true);
+  // channel count
+  view.setUint16(22, 1, true);
+  // sample rate
+  view.setUint32(24, sampleRate, true);
+  // byte rate (sample rate * block align)
+  view.setUint32(28, sampleRate * 2, true);
+  // block align (channel count * bytes per sample)
+  view.setUint16(32, 2, true);
+  // bits per sample
+  view.setUint16(34, 16, true);
+  // data chunk identifier
+  writeString(view, 36, 'data');
+  // data chunk length
+  view.setUint32(40, interleaved.length * 2, true);
+
+  // write the PCM samples
+  const offset = 44;
+  for (let i = 0; i < interleaved.length; i++, offset += 2) {
+    const sample = Math.max(-1, Math.min(1, interleaved[i]));
+    view.setInt16(offset, sample < 0 ? sample * 0x8000 : sample * 0x7FFF, true);
+  }
+
+  return buffer;
+}
+
+function writeString(view, offset, string) {
+  for (let i = 0; i < string.length; i++) {
+    view.setUint8(offset + i, string.charCodeAt(i));
+  }
+}
+
+// Usage example:
+async function processAudio(blob) {
+  try {
+    const wavBlob = await convertBlobToWav(blob); // Wait for the promise to resolve
+    sendAudioToAPI(wavBlob);
+  } catch (error) {
+    console.error('Error converting audio to WAV:', error);
+  }
+}
+
+// Pass the blob to the processAudio function
+    //processAudio(blob);
+    
+    function sendAudioToAPI(blob) {
+    // Create a new FormData object
+    var formData = new FormData();
+
+    // Append the recorded audio blob to the FormData object
+    formData.append('recording', blob);
+
+    // Make an HTTP POST request to the Flask API
+      fetch('http://127.0.0.1:5000/record', {
+        method: 'POST',
+        body: formData
+      })
+        .then(function (response) {
+          // Handle the response from the API
+          if (response.ok) {
+            return response.json();
+          } else {
+            return response.text().then(function (error) {
+      console.error('API request failed:', error);
+      throw new Error('API request failed: ' + error);
+    });
+        }
+    })
+    .then(function(data) {
+        // Process the API response
+        handleApiResponse(data);
+    })
+    .catch(function(error) {
+        // Handle any errors that occur during the API request
+        console.error(error);
+    });
+    }
+
+    function handleApiResponse(data) {
+    // Handle the response data and update the webpage accordingly
+    // You can access the outcome and likelihood values from the 'data' object
+    var outcome = data.outcome;
+    var likelihood = data.likelihood;
+
+    // Update the webpage with the outcome and likelihood information
+    // For example, you can update a <div> element with the results
+    var resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = 'Outcome: ' + outcome + '<br>Likelihood: ' + likelihood + '%';
+}
+
 $(document).ready(function() {
     
     //activate wow.js
@@ -27,133 +194,125 @@ $(document).ready(function() {
 
 // Recording
 
+var msg_box = document.getElementById('msg_box'),
+    button = document.getElementById('button'),
+    canvas = document.getElementById('canvas'),
+    lang = {
+      'mic_error': 'Error accessing the microphone',
+      'recording': 'Recording',
+      'play': 'Play',
+      'stop': 'Stop',
+      'download': 'Download',
+      'use_https': 'This application is not working over an insecure connection. Try to use HTTPS'
+    },
+    time;
 
-
-var msg_box = document.getElementById( 'msg_box' ),
-    button = document.getElementById( 'button' ),
-    canvas = document.getElementById( 'canvas' ),
-lang = {
-    'mic_error': 'Error accessing the microphone', 
-    // 'press_to_start': 'Press to start recording', 
-    'recording': 'Recording', 
-    'play': 'Play', 
-    'stop': 'Stop', 
-    'download': 'Download', 
-    'use_https': 'This application in not working over insecure connection. Try to use HTTPS'
-},
-time;
-
-
-// msg_box.innerHTML = lang.press_to_start;
-
-if ( navigator.mediaDevices === undefined ) {
-    navigator.mediaDevices = {};
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
 }
 
-
-if ( navigator.mediaDevices.getUserMedia === undefined ) {
-    navigator.mediaDevices.getUserMedia = function ( constrains ) {
-        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
-        if ( !getUserMedia )  {
-            return Promise.reject( new Error( 'getUserMedia is not implemented in this browser' ) );
-        }
-
-        return new Promise( function( resolve, reject ) {
-            getUserMedia.call( navigator, constrains, resolve, reject );
-        } );
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function(constrains) {
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
     }
+
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constrains, resolve, reject);
+    });
+  };
 }
 
+if (navigator.mediaDevices.getUserMedia) {
+  var btn_status = 'inactive',
+      mediaRecorder,
+      chunks = [],
+      audio = new Audio(),
+      mediaStream,
+      audioSrc,
+      type = {
+        'type': 'audio/wav'
+      },
+      ctx,
+      analys,
+      blob;
 
-if ( navigator.mediaDevices.getUserMedia ) {
-    var btn_status = 'inactive',
-        mediaRecorder,
-        chunks = [],
-        audio = new Audio(),
-        mediaStream,
-        audioSrc,
-        type = {
-            'type': 'audio/ogg,codecs=opus'
-        },
-        ctx,
-        analys,
-        blob;
-
-    button.onclick = function () {
-        if ( btn_status == 'inactive' ) {
-            start();
-        } else if ( btn_status == 'recording' ) {
-            stop();
-        }
+  button.onclick = function() {
+    if (btn_status == 'inactive') {
+      start();
+    } else if (btn_status == 'recording') {
+      stop();
     }
+  };
 
-    function parseTime( sec ) {
-        var h = parseInt( sec / 3600 );
-        var m = parseInt( sec / 60 );
-        var sec = sec - ( h * 3600 + m * 60 );
+  function parseTime(sec) {
+    var h = parseInt(sec / 3600);
+    var m = parseInt(sec / 60);
+    sec = sec - (h * 3600 + m * 60);
 
-        h = h == 0 ? '' : h + ':';
-        sec = sec < 10 ? '0' + sec : sec;
+    h = h == 0 ? '' : h + ':';
+    sec = sec < 10 ? '0' + sec : sec;
 
-        return h + m + ':' + sec;
-    }
+    return h + m + ':' + sec;
+  }
 
+   function start() {
+    navigator.mediaDevices.getUserMedia({
+      'audio': true
+    }).then(function (stream) {
+      mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
 
-    function start() {
-        navigator.mediaDevices.getUserMedia( { 'audio': true } ).then( function ( stream ) {
-            mediaRecorder = new MediaRecorder( stream );
-            mediaRecorder.start();
+      button.classList.add('recording');
+      btn_status = 'recording';
 
-            button.classList.add( 'recording' );
-            btn_status = 'recording';
+      msg_box.innerHTML = lang.recording;
 
-            msg_box.innerHTML = lang.recording;
-          
-            if ( navigator.vibrate ) navigator.vibrate( 150 );
+      if (navigator.vibrate) navigator.vibrate(150);
 
-            time = Math.ceil( new Date().getTime() / 1000 );
+      time = Math.ceil(new Date().getTime() / 1000);
 
+      mediaRecorder.ondataavailable = function (event) {
+        chunks.push(event.data);
+      };
 
-            mediaRecorder.ondataavailable = function ( event ) {
-                chunks.push( event.data );
-            }
-
-            mediaRecorder.onstop = function () {
-                stream.getTracks().forEach( function( track ) { track.stop() } );
-
-                blob = new Blob( chunks, type );
-                audioSrc = window.URL.createObjectURL( blob );
-
-                audio.src = audioSrc;
-
-                chunks = [];
-            }   
-
-            
-            
-        } ).catch( function ( error ) {
-            if ( location.protocol != 'https:' ) {
-              msg_box.innerHTML = lang.mic_error + '<br>'  + lang.use_https;
-            } else {
-              msg_box.innerHTML = lang.mic_error; 
-            }
-            button.disabled = true;
+      mediaRecorder.onstop = function () {
+        stream.getTracks().forEach(function (track) {
+          track.stop();
         });
-    }
 
-    function stop() {
-        mediaRecorder.stop();
-        button.classList.remove( 'recording' );
-        btn_status = 'inactive';
-      
-        if ( navigator.vibrate ) navigator.vibrate( [ 200, 100, 200 ] );
+        blob = new Blob(chunks, type);
+        audioSrc = window.URL.createObjectURL(blob);
 
-        var now = Math.ceil( new Date().getTime() / 1000 );
+        audio.src = audioSrc;
 
-        var t = parseTime( now - time );
+        chunks = [];
+      };
+    }).catch(function (error) {
+      if (location.protocol != 'https:') {
+        msg_box.innerHTML = lang.mic_error + '<br>' + lang.use_https;
+      } else {
+        msg_box.innerHTML = lang.mic_error;
+      }
+      button.disabled = true;
+    });
+  }
 
-        msg_box.innerHTML = '<a href="#" onclick="play(); return false;" class="txt_btn">' + lang.play + ' (' + t + 's)</a><br>' +
-                            '<a href="#" onclick="save(); return false;" class="txt_btn">' + lang.download + '</a>'
+  function stop() {
+    mediaRecorder.stop();
+    button.classList.remove('recording');
+    btn_status = 'inactive';
+
+    if (navigator.vibrate) navigator.vibrate([200, 100, 200]);
+
+    var now = Math.ceil(new Date().getTime() / 1000);
+
+    var t = parseTime(now - time);
+
+    msg_box.innerHTML = '<a href="#" onclick="play(); return false;" class="txt_btn">' + lang.play  + ' (' + t + 's)</a><br>' +
+        '<a href="#" onclick="save(); return false;" class="txt_btn">' + lang.download + '</a>'
+      sendAudioToAPI(blob)
     }
 
     
